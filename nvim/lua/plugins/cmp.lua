8ad7ed3cf8647bd802df472b1d9d8939a1fd1a29
Daniel Jin <mrpickles123115@gmail.com>
2025-08-02 16:38:34 -0600

    question mark

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -47,3 +47,9 @@ cmp.setup {
     { name = 'path' }
   },
 }
+
+-- Set this check up for nvim-cmp tab mapping
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
