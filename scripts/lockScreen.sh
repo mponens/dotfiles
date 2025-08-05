5804987326a3b62855f223de4997e73ca8afc07b
Daniel Jin <mrpickles123115@gmail.com>
2025-08-05 13:58:15 -0600

    change env

--- scripts/lockScreen.sh
+++ scripts/lockScreen.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo "-i /home/danielj/Pictures/plant.jpg --effect-vignette 0.3:1 -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1"
+echo "-i $HOME/Pictures/plant.jpg --effect-vignette 0.3:1 -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1"
 
 swaylock -f -i /home/danielj/Pictures/plant.jpg --effect-vignette 0.3:1 -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1
 
