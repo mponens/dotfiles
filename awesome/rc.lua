056ae0c9ed1c6c9379301e0165c433e1d7510ad8
Daniel Jin <mrpickles123115@gmail.com>
2025-08-25 22:11:27 -0400

    remove rules

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -403,46 +403,7 @@ awful.rules.rules = {
 		}
 	},
 
-	-- Floating clients.
-	{
-		rule_any = {
-			instance = {
-				"DTA", -- Firefox addon DownThemAll.
-				"copyq", -- Includes session name in class.
-				"pinentry",
-			},
-			class = {
-				"Arandr",
-				"Blueman-manager",
-				"Gpick",
-				"Kruler",
-				"MessageWin", -- kalarm.
-				"Sxiv",
-				"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-				"Wpa_gui",
-				"veromix",
-				"xtightvncviewer" },
 
-			-- Note that the name property shown in xprop might be set slightly after creation of the client
-			-- and the name shown there might not match defined rules here.
-			name = {
-				"Event Tester", -- xev.
-			},
-			role = {
-				"AlarmWindow", -- Thunderbird's calendar.
-				"ConfigManager", -- Thunderbird's about:config.
-				"pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
-			}
-		},
-		properties = { floating = true }
-	},
-
-	-- Remove titlebars from normal clients and dialogs
-	{
-		rule_any = { type = { "normal", "dialog" }
-		},
-		properties = { titlebars_enabled = false }
-	},
 
 	-- Set Firefox to always map on the tag named "2" on screen 1.
 	-- { rule = { class = "Firefox" },
