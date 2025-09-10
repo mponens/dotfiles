a29ea4828cd978a406295580201537df928cba79
Daniel Jin <mrpickles123115@gmail.com>
2025-09-10 17:21:36 -0400

    use sops secrets

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,17 +1,27 @@
 require("codecompanion").setup({
-  display = {
-	  chat = {
+	display = {
+		chat = {
 			window = {
 				position = "right",
 			},
 		},
 	},
-  strategies = {
-    chat = {
-      adapter = "gemini",
-    },
-    inline = {
-      adapter = "gemini",
-    },
-  },
+	strategies = {
+		chat = {
+			adapter = "gemini",
+		},
+		inline = {
+			adapter = "gemini",
+		},
+	},
+	adapters = {
+		gemini = function()
+			return require("codecompanion.adapters").extend("gemini", {
+				env = {
+					api_key = "cmd:cat /run/secrets/api-keys/gemini-api-key",
+				},
+			})
+		end,
+	},
+
 })
