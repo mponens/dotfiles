4a6aea8a0025e51fa9b60db044bc5c64393c7778
Daniel Jin <mrpickles123115@gmail.com>
2025-11-15 22:40:58 -0500

    have nvim-tree show current file

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,1 +1,8 @@
-require('nvim-tree').setup({view = {relativenumber = true}})
+require('nvim-tree').setup({
+	view = { relativenumber = true },
+	update_focused_file = {
+		enable = true,
+		update_root = true, -- Optional: updates the root of the tree to the current file's directory
+		ignore_list = {}, -- Optional: list of file patterns to ignore
+	},
+})
