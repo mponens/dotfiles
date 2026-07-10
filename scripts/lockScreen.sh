19ea3fba321ee5879494c7df126bc4c193a322bc
Daniel Jin <mrpickles123115@gmail.com>
2026-07-10 07:06:23 -0400

    remove unused

--- scripts/lockScreen.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo "-i $HOME/Pictures/plant.jpg --effect-vignette 0.3:1 -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1"
-
-swaylock -f -i /home/danielj/Pictures/plant.jpg --effect-vignette 0.3:1 -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1
-
-#swaylock -f -i /home/danielj/Pictures/bsod.jpg -r --ring-color 132E6977 --bs-hl-color 19CCBA --key-hl-color 49706F77 --ring-ver-color 132E6977 --inside-ver-color 132E6922 $1
-
