0d96803386892777e2bac5f7af7b0b740c6e7014
Daniel Jin <mrpickles123115@gmail.com>
2026-07-10 06:29:18 -0400

    fix detection

--- nvim/lua/utils/nix.lua
+++ nvim/lua/utils/nix.lua
@@ -1,9 +1,6 @@
 local M = {}
 
 function M.is_nix_managed()
-  return vim.g.nix_managed_plugins or 
-         vim.fn.has('nixos') == 1 or 
-         os.getenv('NIX_STORE') ~= nil or
-         vim.fn.filereadable('/etc/NIXOS') == 1
+  return vim.g.nix_managed_plugins == true
 end
 return M
